3d9006ad12f78f4634097650f8e00e4e581a26b6
Jamshid91 <iymon0217@gmail.com>
2022-04-08 23:24:43 +0500

    added

---
+++ assets/js/index.js
@@ -0,0 +1,42 @@
+let navLine = document.querySelector('.nav-line'),
+    menuBurger = document.querySelector('.nav-menu-burger'),
+    closeMenu = document.querySelector('.close-menu'),
+    menuItem = document.querySelector('.menu-item'),
+    sliders = document.querySelectorAll('.slider-item')
+
+
+window.onload = function() {
+    setTimeout(() => {
+        navLine.classList.add('showLine')
+    }, 1000);
+}
+
+
+menuBurger.addEventListener('click', () => {
+    menuItem.classList.add('showMenu')
+});
+
+closeMenu.addEventListener('click', () => {
+    menuItem.classList.remove('showMenu')
+});
+
+
+let i = 0;
+
+function sliderMove() {
+    if (i == sliders.length - 1) {
+        sliders[i].style.display = "none";
+        i = 0;
+        sliders[i].style.display = "block";
+    } else {
+        sliders[i].style.display = "none";
+        sliders[i + 1].style.display = "block";
+        i++
+    }
+}
+
+
+
+setInterval(function () {
+    sliderMove();
+}, 2000)
